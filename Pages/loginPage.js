6bc598423a3c66e0949b8436f565d19983fe7c6b
MHDOA <mohammadali4a@gmail.com>
2024-08-27 07:54:30 +0330

    Add login page
    add login page in 'Pages' folder and its style and set default layout in index.js file

---
+++ Pages/loginPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import loginStyle from '../Styles/loginPageStyle';
+import {
+  Button, Heading, NativeBaseProvider, Text, View, VStack, HStack, Input, Center
+} from 'native-base'
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <NativeBaseProvider>
+
+      </NativeBaseProvider>
+    );
+  }
+}
+
+export default Login;
---
+++ Styles/loginPageStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const loginStyle = StyleSheet.create({
+
+    TitleStyle:{
+        fontSize:15,
+        fontWeight:'bold'
+    },
+    HeaderStyle:{
+        flex:1,
+        backgroundColor:"#000"
+    },
+    BodyStyle:{
+        flex:14,
+        alignItems:'center',
+        justifyContent:'flex-start'
+    },
+    FooterStyle:{
+        flex:1,
+        backgroundColor:"#0f0"
+    }
+})
+
+export default loginStyle
