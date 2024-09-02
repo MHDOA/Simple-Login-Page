23fe49b464b04b240a2bec520744ba0bffcf372c
MHDOA <mohammadali4a@gmail.com>
2024-09-02 15:40:54 +0330

    Optimize Validation Method
    change model of errors and make eat shorter and use an ErrorText to check error exist and print error

--- Components/LoginCardView.js
+++ Components/LoginCardView.js
@@ -1,40 +1,49 @@
 import React, { Component, useState } from 'react';
 import { Box, Input, FormControl, Button, Icon } from 'native-base';
 import { IconFill, IconOutline } from "@ant-design/icons-react-native";
+import { Alert } from 'react-native';
 export default LoginCardView = props => {
 
-    const Usernames = ['محمد', 'علی']
+    const VALIDATE_USERNAME = ['علی', 'محمد']
+
+    const MIN_USERNAME_LENGTH = 3
+    const MAX_USERNAME_LENGTH = 24
+    const MIN_PASSWORD_LENGTH = 8
+    const MAX_PASSWORD_LENGTH = 32
 
     const [username, SetUsername] = useState('')
     const [password, SetPassword] = useState('')
+    const [showPassword, SetShowPassword] = useState(false)
+    const [usernameError, SetUsernameError] = useState(null)
+    const [passwordError, SetPasswordError] = useState(null)
 
-    const [isShowPassword, SetIsShowPassword] = useState(false)
-    const [isUsernameCorrectFormat, SetIsUsernameCorrectFormat] = useState(true)
-    const [isPasswordCorrectFormat, SetIsPasswordCorrectFormat] = useState(true)
-    const [isUsernameExist, SetIsUsernameExist] = useState(true)
-    const [isCorrectPassword, SetIsCorrectPassword] = useState(true)
-
-
-    const IsInvalid = (type) => {
-        switch (type) {
-            case 'username': {
-                // return(!isUsernameCorrectFormat || !isUsernameExist)
-                return (!isUsernameCorrectFormat)
-            }
-
-            case 'password': {
-                return (!isPasswordCorrectFormat || !isCorrectPassword)
-            }
-            default: {
-                return false
-            }
+    const UsernameValidation = () => {
+        if (username.length < MIN_USERNAME_LENGTH || username.length > MAX_USERNAME_LENGTH) {
+            return 'طول نام کاربری باید بین ۳ تا ۲۴ کاراکتر باشد'
         }
+        // else if (!VALIDATE_USERNAME.includes(username)) {
+        //     return 'نام کاربری موجود نمیباشد'
+        // }
+        return null
     }
 
-    const LoginButtonClick = () => {
-        SetIsUsernameCorrectFormat((username.length < 3 || username.length > 24 ? false : true))
-        SetIsPasswordCorrectFormat((password.length < 8 || password.length > 32 ? false : true))
-        SetIsUsernameExist((Usernames.find(x => x == username) ? true : false))
+    const PasswordValidation = () => {
+        if (password.length < MIN_PASSWORD_LENGTH || password.length > MAX_PASSWORD_LENGTH) {
+            return 'طول رمز عبور باید بین ۸ تا ۳۲ کاراکتر باشد'
+        }
+        return null
+    }
+
+    const HandleLogin = () => {
+        const usernameValidationError = UsernameValidation()
+        const passwordValidationError = PasswordValidation()
+
+        SetUsernameError(usernameValidationError)
+        SetPasswordError(passwordValidationError)
+
+        if (!usernameValidationError && !passwordValidationError) {
+            // Successfull login       
+        }
     }
 
     return (
@@ -42,32 +51,87 @@ export default LoginCardView = props => {
 
             <Box alignItems='center' w='90%'>
 
-                <FormControl isInvalid={IsInvalid('username')} mt='5'>
+                <FormControl isInvalid={!!usernameError} mt='5'>
                     <FormControl.Label justifyContent='flex-end'>نام کاربری</FormControl.Label>
 
-                    <Input variant='underlined' textAlign='right' placeholder='نام کاربری' isRequired={true} onChangeText={SetUsername} value={username}
-                        InputRightElement={<IconFill name='idcard' size={18} color='#BEBFC5' style={{ marginRight: 5 }} />} />
+                    <Input
+                        variant='underlined'
+                        textAlign='right'
+                        placeholder='نام کاربری'
+                        isRequired={true}
+                        onChangeText={SetUsername}
+                        value={username}
+                        InputRightElement=
+                        {<IconFill
+                            name='idcard'
+                            size={18}
+                            color='#BEBFC5'
+                            style={{ marginRight: 5 }}
+                        />}
+                    />
 
-                    <FormControl.ErrorMessage alignItems='flex-end' rightIcon={<IconOutline name='info-circle' color='#f00' size={10} />}>
-                        {isUsernameCorrectFormat ? null : 'نام کاربری باید از 3 تا 24 کاراکتر باشد'}
-                        {/* {isUsernameExist || !isUsernameCorrectFormat? null : 'نام کاربری یافت نشد'} */}
+                    <FormControl.ErrorMessage
+                        alignItems='flex-end'
+                        rightIcon=
+                        {<IconOutline
+                            name='info-circle'
+                            color='#f00' size={10}
+                        />}
+                    >
+                        {usernameError}
                     </FormControl.ErrorMessage>
+
                 </FormControl>
 
 
-                <FormControl isInvalid={IsInvalid('password')} mt='5'>
+                <FormControl isInvalid={!!passwordError} mt='5'>
                     <FormControl.Label justifyContent='flex-end'>رمز ورود</FormControl.Label>
 
-                    <Input variant='underlined' textAlign='right' placeholder='رمز ورود' isRequired={true} onChangeText={SetPassword} value={password} type={isShowPassword ? 'text' : 'password'}
-                        InputLeftElement={<IconFill name={isShowPassword ? 'eye' : 'eye-invisible'} size={18} color='#242124' style={{ marginLeft: 5 }} onPress={() => SetIsShowPassword(!isShowPassword)} />}
-                        InputRightElement={<IconFill name='lock' size={18} color='#BEBFC5' style={{ marginRight: 5 }} />} />
+                    <Input
+                        variant='underlined'
+                        textAlign='right'
+                        placeholder='رمز ورود'
+                        isRequired={true}
+                        onChangeText={SetPassword}
+                        value={password}
+                        type={showPassword ? 'text' : 'password'}
 
-                    <FormControl.ErrorMessage alignItems='flex-end' rightIcon={<IconOutline name='info-circle' color='#f00' size={10} />}>
-                        {isPasswordCorrectFormat ? null : 'رمز عبور باید از 8 تا 32 کاراکتر باشد'}
+                        InputLeftElement=
+                        {<IconFill
+                            name={showPassword ? 'eye' : 'eye-invisible'}
+                            size={18} color='#242124' style={{ marginLeft: 5 }}
+                            onPress={() => SetShowPassword(!showPassword)}
+                        />}
+
+                        InputRightElement=
+                        {<IconFill
+                            name='lock'
+                            size={18}
+                            color='#BEBFC5'
+                            style={{ marginRight: 5 }}
+                        />}
+                    />
+
+                    <FormControl.ErrorMessage
+                        alignItems='flex-end'
+                        rightIcon=
+                        {<IconOutline
+                            name='info-circle'
+                            color='#f00'
+                            size={10}
+                        />}
+                    >
+                        {passwordError}
                     </FormControl.ErrorMessage>
+
                 </FormControl>
 
-                <Button w='75%' mt='5' onPress={LoginButtonClick}>ورود</Button>
+                <Button
+                    w='75%'
+                    mt='5'
+                    onPress={HandleLogin}>
+                    ورود
+                </Button>
 
             </Box>
 
